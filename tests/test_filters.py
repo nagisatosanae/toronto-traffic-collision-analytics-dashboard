81cf4bbb07838f0536a51d62cafac74aca5c993c
Sunghoon Ahn <andy.ahn1978@gmail.com>
2026-03-17 13:28:48 -0400

    RED: add failing test for year filter

---
+++ tests/test_filters.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from src.filters import filter_by_year
+
+
+def test_filter_by_year_returns_only_selected_year():
+    df = pd.DataFrame({
+        "OCC_YEAR": [2022, 2023, 2023],
+        "VALUE": [10, 20, 30]
+    })
+
+    result = filter_by_year(df, 2023)
+
+    assert len(result) == 2
+    assert set(result["VALUE"]) == {20, 30}
