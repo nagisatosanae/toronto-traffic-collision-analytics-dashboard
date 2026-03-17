02deae7fdaef71186001db9b75cc9d866fbfd19f
espark <pia2carrot@naver.com>
2026-03-17 17:31:54 -0400

    [#7] RED: Add failing tests for hourly collision chart

---
+++ src/hourly_visualization.py
@@ -0,0 +1,9 @@
+"""Hourly collision trend visualization module."""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def create_hourly_chart(df: pd.DataFrame) -> plt.Figure:
+    """Create a bar chart showing collisions by hour."""
+    pass
