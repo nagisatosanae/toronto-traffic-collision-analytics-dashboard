6185215c8314e396d22bface7b3a22cd52f926d7
espark <pia2carrot@naver.com>
2026-03-17 17:32:40 -0400

    [#7] GREEN: Implement hourly collision chart

--- src/hourly_visualization.py
+++ src/hourly_visualization.py
@@ -6,4 +6,9 @@ import matplotlib.pyplot as plt
 
 def create_hourly_chart(df: pd.DataFrame) -> plt.Figure:
     """Create a bar chart showing collisions by hour."""
-    pass
+    fig, ax = plt.subplots()
+    ax.bar(df["OCC_HOUR"], df["collision_count"])
+    ax.set_title("Collisions by Hour of Day")
+    ax.set_xlabel("Hour")
+    ax.set_ylabel("Number of Collisions")
+    return fig
