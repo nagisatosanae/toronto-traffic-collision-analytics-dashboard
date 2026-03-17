a80ccdbf413f2041b648a1238021f5291f5076fc
Sunghoon Ahn <andy.ahn1978@gmail.com>
2026-03-17 13:47:04 -0400

    RED: add failing test for severity filter

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from src.filters import filter_by_year
+from src.filters import filter_by_severity
 
 
 def test_filter_by_year_returns_only_selected_year():
@@ -12,3 +13,13 @@ def test_filter_by_year_returns_only_selected_year():
 
     assert len(result) == 2
     assert set(result["VALUE"]) == {20, 30}
+
+def test_filter_by_severity_returns_only_selected_level():
+    df = pd.DataFrame({
+        "ACCLASS": ["Fatal", "Non-Fatal", "Fatal"],
+        "VALUE": [1, 2, 3]
+    })
+
+    result = filter_by_severity(df, "Fatal")
+
+    assert len(result) == 2
